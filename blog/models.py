0c62fda36d656fab814747faeddb203e97f60552
Jongdae Han <elvenwhite@smu.ac.kr>
2022-03-22 01:45:35 +0900

    미디어/스태틱 처리, 헤더 이미지 및 첨부기능 추가

--- blog/models.py
+++ blog/models.py
@@ -6,9 +6,16 @@ class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
 
+    head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
+    file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d/', blank=True)
+
+
     create_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     # 모델 메소드 정의(오버라이드)
     def __str__(self):
         return f'[{self.pk}]   [{self.title}]'
+
+    def get_absolute_url(self):
+        return f'/blog/{self.pk}/'
--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,28 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Post
+
+
 # Create your views here.
 
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
 
-def index(request):
-    posts = Post.objects.all()
-    return render(request, 'blog/index.html',
-                  {
-                      'posts' : posts,
-                  }
-    )
+class PostDetail(DetailView):
+    model = Post
+
+#
+# def index(request):
+#     posts = Post.objects.all()
+#     return render(request, 'blog/index.html',
+#                   {
+#                       'posts' : posts,
+#                   }
+#     )
+#
+#
+# def single_post_page(request, pk):
+#     post = Post.objects.get(pk = pk)
+#
+#
+#     return render(request, 'blog/post_detail.html', {'post':post})
