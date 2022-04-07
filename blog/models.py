232cbd36fd5ac82116a407d51f05121c70d1793a
Jongdae Han <elvenwhite@smu.ac.kr>
2022-04-08 05:13:24 +0900

    카테고리 모델 추가 및 post에 연결

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,23 @@
 import os.path
 
 from django.contrib.auth.models import User
+from django.core.management.commands.makemessages import plural_forms_re
 from django.db import models
 
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural : 'Categories'
+
+
 class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
@@ -13,8 +26,9 @@ class Post(models.Model):
     file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d/', blank=True)
 
     author = models.ForeignKey(User, on_delete=models.CASCADE )
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
 
-    create_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     # 모델 메소드 정의(오버라이드)
@@ -26,3 +40,5 @@ class Post(models.Model):
 
     def get_file_name(self):
         return os.path.basename(self.file_upload.name)
+
+
