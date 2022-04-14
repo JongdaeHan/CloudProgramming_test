5eaddacf8abd0bbe4fbd1e12128799cf1b6965a7
Jongdae Han <elvenwhite@smu.ac.kr>
2022-04-15 00:14:57 +0900

    post 작성 뷰 생성

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Post, Category, Tag
 
 
 # Create your views here.
+
+class PostCreate(CreateView):
+    model=Post
+    fields = ['title', 'content', 'head_image', 'file_upload', 'category']
+
 
 class PostList(ListView):
     model = Post
